9a675e0c14553c78454a2ae5f0581b8af9212f7b
Vasilije Kiso <kisovasilije46@gmail.com>
2025-10-18 13:24:38 +0200

    user-profile: add user to db

--- app/src/main/java/com/example/rpg/ui/fragments/RegistrationFragment.java
+++ app/src/main/java/com/example/rpg/ui/fragments/RegistrationFragment.java
@@ -1,10 +1,12 @@
 package com.example.rpg.ui.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.room.Room;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.example.rpg.R;
+import com.example.rpg.database.AppDatabase;
 import com.example.rpg.databinding.FragmentRegistrationBinding;
+import com.example.rpg.model.Avatar;
+import com.example.rpg.model.User;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Locale;
+import java.util.concurrent.Executors;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -67,7 +76,61 @@ public class RegistrationFragment extends Fragment {
 
     private void registerRegisterButton() {
         binding.registerButton.setOnClickListener(v -> {
-            Toast.makeText(requireContext(), "Successfully registered.", Toast.LENGTH_SHORT).show();
+            var user = cvtBindingToUser(v);
+            if (user == null) return;
+
+            AppDatabase db = Room.databaseBuilder(
+                    requireContext().getApplicationContext(),
+                    AppDatabase.class,
+                    AppDatabase.databaseName
+            ).build();
+
+            new Thread(() -> {
+                var userId = db.userDao().insert(user);
+
+                requireActivity().runOnUiThread(() -> {
+                    Toast.makeText(
+                            requireContext(),
+                            String.format(Locale.US, "User %d successfully registered.", userId),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                });
+            }).start();
         });
+    }
+
+    private User cvtBindingToUser(View v) {
+        var password = binding.passwordInput.getText().toString();
+        if (!password.equals(binding.confirmPasswordInput.getText().toString())) {
+            Snackbar
+                    .make(v, "Password mismatch.", Snackbar.LENGTH_SHORT)
+                    .setBackgroundTint(Color.RED)
+                    .show();
+
+            return null;
+        }
+
+        Avatar a;
+        var in = binding.avatarInput.getSelectedItem().toString();
+        var parsedAvatar = in.replace(" ", "").toUpperCase();
+
+        try {
+            a = Avatar.valueOf(parsedAvatar);
+        }
+        catch (Exception e) {
+            Snackbar
+                    .make(v, (e.getMessage() != null) ? e.getMessage() : "Error occurred selecting avatar.", Snackbar.LENGTH_SHORT)
+                    .setBackgroundTint(Color.RED)
+                    .show();
+
+            return null;
+        }
+
+        return new User(
+                binding.emailInput.getText().toString(),
+                binding.usernameInput.getText().toString(),
+                password,
+                a
+        );
     }
 }
