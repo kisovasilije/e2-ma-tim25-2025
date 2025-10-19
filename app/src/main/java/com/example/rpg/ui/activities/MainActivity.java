fb6a8567354e30c8e0b917a0225c278053820bdf
Vasilije Kiso <kisovasilije46@gmail.com>
2025-10-19 08:24:37 +0200

    auth: add home and clean up main activity

--- app/src/main/java/com/example/rpg/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/rpg/ui/activities/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.rpg.ui.activities;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,8 +14,6 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.rpg.R;
 import com.example.rpg.databinding.ActivityMainBinding;
-import com.example.rpg.ui.fragments.AuthFragment;
-import com.example.rpg.ui.fragments.RegistrationFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -66,30 +60,5 @@ public class MainActivity extends AppCompatActivity {
 
         var navController = Navigation.findNavController(this, R.id.nav_host);
         NavigationUI.setupWithNavController(binding.navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        /*if (id == R.id.nav_signup) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new RegistrationFragment())
-                    .commit();
-        }
-
-        if (id == R.id.nav_login) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new AuthFragment())
-                    .commit();
-        }*/
-
-        return super.onOptionsItemSelected(item);
     }
 }
